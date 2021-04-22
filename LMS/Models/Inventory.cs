73ac0bcfa1c9e40b79d2206006e1796842e96d45
josemattam <josesebanm@gmail.com>
2021-04-22 17:10:30 -0600

    scaffolded the database

---
+++ LMS/Models/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models
+{
+    public partial class Inventory
+    {
+        public uint Serial { get; set; }
+        public string Isbn { get; set; }
+
+        public virtual Titles IsbnNavigation { get; set; }
+        public virtual CheckedOut CheckedOut { get; set; }
+    }
+}
